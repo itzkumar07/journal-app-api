dba7dd1518f8b5ba51a0b9ecc500e46cf0db503b
Kumar Kankam <112927086+itzkumar07@users.noreply.github.com>
2025-04-23 16:47:05 +0530

    Initial commit

---
+++ src/main/java/com/bootcoding/journal_app_api/JournalAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.bootcoding.journal_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcoding/journal_app_api/JournalAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcoding.journal_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
